ae75d0e85b15406eb36f84735ca5c60e9be1440e
Psycojoee <marthi480@gmail.com>
2023-09-01 09:39:59 -0300

    localstorage

--- src/app/componentes/Cardfilme.jsx
+++ src/app/componentes/Cardfilme.jsx
@@ -1,7 +1,45 @@
+"use client"
+import { useEffect, useState } from "react"
+import { HeartIcon } from '@heroicons/react/24/solid'
+import { HeartIcon as HeartIconOutline } from '@heroicons/react/24/outline'
+
+
 export default function Cardfilme({filme}){
+    const [ favorito, setFavorito ] = useState(false) //hooks
+
+    useEffect( () => {
+        let favoritos = JSON.parse (localStorage.getItem ("favoritos") ) || []
+        const favorito = favoritos.find( f => f.id === filme.id)
+        setFavorito(favorito)
+    }, [] )
+
+    function favoritar(){
+        setFavorito(true)
+        let favoritos = JSON.parse (localStorage.getItem ("favoritos") ) || []
+        favoritos.push(filme)
+        localStorage.setItem("favoritos", JSON.stringify (favoritos))
+    }
+    function desfavoritar(){
+        setFavorito(false)
+        let favoritos = JSON.parse (localStorage.getItem ("favoritos") ) || []
+        const favoritosAtualizado = favoritos.filter(f => f.id !== filme.id)
+        localStorage.setItem("favoritos", JSON.stringify (favoritosAtualizado))
+    }
+
     return (
-        <div className="flex flex-col gap-1 items-center w-40 m-2">
-            <img className = "rounded" src={filme.poster} alt="Poster do Filme" />
+        <div className="flex flex-col items-center justfify-between gap-1 w-40 m-2 relative">
+            {favorito ?
+                <HeartIcon
+                    className="h-6 w-6 text-red-600 absolute top-1 right-2 cursor-pointer hover:text-white"
+                    onClick={desfavoritar}
+                 />
+                :
+                <HeartIconOutline
+                    className="h-6 w-6 absolute top-1 right-2 cursor-pointer hover:text-red-600"
+                    onClick={favoritar}
+                />                
+            }
+            <img className = "rounded h-56" src={filme.poster} alt="Poster do Filme" />
             <span className="font-bold text-lg w-full line-clamp-1 text-center">{filme.titulo}</span>
             <div className="flex items-center gap-2">
                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 text-amber-500">
--- src/app/page.js
+++ src/app/page.js
@@ -7,11 +7,13 @@ export default function Home() {
   //mock
   const filmes = [
     {
+      id: 1,
       titulo: "Star Wars",
       nota: 9.5,
       poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/vOUxo5sfXyQPIoF8rtYK1OVVBPi.jpg"
     },
     {
+      id: 2,
       titulo: "Barbie",
       nota: 9.0,
       poster: "https://www.themoviedb.org/t/p/w94_and_h141_bestv2/yRRuLt7sMBEQkHsd1S3KaaofZn7.jpg"
@@ -21,8 +23,8 @@ export default function Home() {
 
  return (
   <>
-   <nav className='bg-slate-900 p-2 flex gap-3 items-end'>
-    <h1 className='holtwood one sc font-bold'>NYA NIMES</h1>
+   <nav className='bg-slate-900 text-zinc-100 p-2 flex gap-3 items-end'>
+    <h1 className='holtwood one sc font-bold '>NYA NIMES</h1>
     <h2 className='holtwood one sc font-bold'>Favoritos</h2>
     <h3 className='holtwood one sc font-bold'>Lançamentos</h3>
     <h4 className='holtwood one sc font-bold'>Sobre</h4>
